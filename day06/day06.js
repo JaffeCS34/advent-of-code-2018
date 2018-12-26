a08f7c11fe2c9a18b14cedcdc426a91166b93af5
rogerjaffe <rogerjaffe@gmail.com>
2018-12-26 04:42:44 +0000

    Added day 8

--- day06/day06.js
+++ day06/day06.js
@@ -22,7 +22,36 @@ fs.readFile(fileSpec, 'utf8', (err, data) => {
   }, {maxX:0, maxY: 0}, coords)
   
   console.log(maxCoords);
+  
+  let closestPoints = R.map(p => R.map(c => 0, R.range(0, maxCoords.maxY)), R.range(0, maxCoords.maxX))
+  const reduceIdx = R.addIndex(R.reduce)
+  const mapIdx = R.addIndex(R.map)
+
   // Part 1
+  closestPoints = mapIdx((row, x) => {
+    return mapIdx((cell, y) => {
+      return reduceIdx((acc, point, idx) => {
+        let dx = getDistance(x, y, point.x, point.y);
+        if (acc.dx === null) {
+          acc.dx = dx;
+          acc.pt = idx
+        } else {
+          if (dx < acc.dx) {
+            acc.dx = dx
+            acc.pt = idx
+          }
+        }
+        return acc
+      }, {dx: null, pt: -1}, coords)
+    }, row)
+  }, closestPoints);
+  
+  let spacesForCells = R.map(a => 0, R.range(0, coords.length))
+  for (let x=0; x<maxCoords.maxX; x++) {
+    for (let y=0; y<maxCoords.maxY; y++) {
+      spacesForCells[closestPoints[x][y].pt]++;
+    }
+  }
 
   // Part 2
 
---
+++ day08/day08.js
@@ -0,0 +1,58 @@
+const fs = require('fs');
+const R = require('ramda');
+const fileSpec = 'data08.txt';
+
+fs.readFile(fileSpec, 'utf8', (err, data) => {
+
+  // Parse data
+  let items = R.split(' ', data);
+  items = R.map(n => parseInt(n,10), items);
+  
+  let metadataSum = 0;
+  
+  const Node = data => {
+    let childNodeCount = data[0];
+    let metadataCount = data[1];
+    let _data = R.takeLast(data.length-2, data);
+    let nodes = [];
+    let metadata = [];
+  
+    for (let cn=0; cn<childNodeCount; cn++) {
+      let nodeData = Node(_data);
+      _data = nodeData.data;
+      nodeData = R.dissoc('data', nodeData);
+      nodes.push(nodeData);
+    }
+    for (let mn=0; mn<metadataCount; mn++) {
+      metadata.push(_data[mn]);
+      metadataSum += _data[mn];
+    }
+    _data = R.takeLast(_data.length-metadataCount, _data);
+    return {
+      data: _data,
+      nodes,
+      metadata,
+      getValue: () => {
+        if (nodes.length === 0) {
+          return R.reduce((acc, md) => acc + md, 0, metadata)
+        } else {
+          return R.reduce((acc, metadata) => {
+            if (metadata <= nodes.length) {
+              return acc + nodes[metadata-1].getValue()
+            } else {
+              return acc;
+            }
+          }, 0, metadata)
+        }
+      }
+    }
+  }
+  
+  let node = Node(items);
+  console.log('Metadata sum: '+metadataSum);
+  console.log('Root node value: '+node.getValue());
+
+  // Part 2
+
+    
+})
